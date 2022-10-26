bebb12b33f5f492669cb3616d4d0232e11f0ee63
unknown <marufmozumder7@gmail.com>
2022-10-26 13:16:19 +0200

    card-added

---
+++ src/Course/Course.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Course.css'
+
+const Course = ({allCourses}) => {
+//    const {id, title, img, details, duration, price} = allCourses;
+    return (
+        <div className='row row-cols-md-1 row-cols-sm-1 row-cols-lg-3 row-cols-1 g-4'>
+        {
+            allCourses.map(course =>
+                     <div className=" col">
+    <div className="card">
+      <img className='card-img-top' src={course.img}></img>
+      <div className="card-body">
+        <h5 className="card-title"> {course.title}</h5>
+        <p className="card-text">{course.details.slice(0,200)+'...'}<Link>Read More</Link></p>
+        <div className='d-flex justify-content-between mx-3'>
+            <div>
+<span> Duration : {course.duration}</span>
+            </div>
+            <div>
+<span>Price: €{course.price}</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+               
+                )
+        }
+        </div>
+    );
+};
+
+export default Course;
--- src/Courses/Courses.js
+++ src/Courses/Courses.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import Course from '../Course/Course';
 
 const Courses = () => {
     const allCourses = useLoaderData();
     console.log(allCourses);
     return (
         <div className='row'>
-             <div className="col-2">
+             <div className="col-lg-2 col-md-12">
                 <ul>
                     {
                         allCourses.map(course => 
@@ -14,8 +15,10 @@ const Courses = () => {
                     }
                 </ul>
              </div>
-    <div className="col-10 bg-secondary">
-        
+    <div className="col-lg-9 col-md-12 ">
+        <div>
+        <Course  key={allCourses.id} allCourses= {allCourses}></Course>
+        </div>
     </div>
         </div>
     );
