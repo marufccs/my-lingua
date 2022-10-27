bf80c0cf4d4e66079d47e47f49ad23cbc61481f3
unknown <marufmozumder7@gmail.com>
2022-10-27 14:31:54 +0200

    final-touch

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Accordion } from 'react-bootstrap';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>This is Blog</h2>
-        </div>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>What is CORS?</Accordion.Header>
+        <Accordion.Body>
+        Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request.For security reasons, browsers restrict cross-origin HTTP requests initiated from scripts. For example, XMLHttpRequest and the Fetch API follow the same-origin policy. This means that a web application using those APIs can only request resources from the same origin the application was loaded from unless the response from other origins includes the right CORS headers.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Why are you using `firebase`? What other options do you have to implement authentication?</Accordion.Header>
+        <Accordion.Body>
+        It's easy to start a project with Firebase or add a Firebase to your project. It allows real-time database connection, which means multiple users can see the changes in the data when the data gets created or edited. Data transmission is handled with web sockets so you don't have to send requests to get new data, you only need to subscribe once.
+The same applies for file storage. Quick setup authentication via the major providers (Google, Twitter, Facebook, GitHub). Https by default - secure http traffic without setting up certificates. Any static html/javascript content can be hosted.
+<p>
+    The other options beside firebase are: Back4App, BackEndLess, Kuzzle, Pubnub etc
+</p>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>How does the private route work?</Accordion.Header>
+        <Accordion.Body>
+        The private route component is similar to the public route, the only change is that redirect URL and authenticate condition.
+
+If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>What is Node? How does Node work?</Accordion.Header>
+        <Accordion.Body>
+        Node.js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
+
+Working of Node.js: Node.js accepts the request from the clients and sends the response, while working with the request node.js handles them with a single thread. To operate I/O operations or requests node.js use the concept of threads. Thread is a sequence of instructions that the server needs to perform. It runs parallel on the server to provide the information to multiple clients. Node.js is an event loop single-threaded language. It can handle concurrent requests with a single thread without blocking it for one request.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
     );
 };
 
--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -6,11 +6,12 @@ import './Checkout.css'
 const Checkout = () => {
     const { user} = useContext(AuthContext);
  const data = useLoaderData();
-const {title} = data;
+const {title, img} = data;
     return (
        <div>
         <h1 className='text-success mt-5 pt-5'>Congratulations {user?.displayName}!!</h1>
         <h3>You finally got the premium access of the {title} course!</h3>
+        <img src={img} alt="" />
        </div>
     );
 };
